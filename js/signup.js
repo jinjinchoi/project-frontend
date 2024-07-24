de8235b24e50ccf7e027d97e424cd84651e38fbe
devingnow <https://github.com/devingnow>
2024-07-24 11:54:29 +0900

    0724 login,signup clear

--- js/signup.js
+++ js/signup.js
@@ -80,6 +80,16 @@ class SignupCheck{
     constructor(){
         this.checkTrue = document.querySelector('#check_id_true')
         this.signupBtn = document.querySelector('#signup_btn')
+        this.pwInput = document.querySelector('#upw')
+        this.pwInput2 = document.querySelector('#check_upw')
+        this.nickInput = document.querySelector('#uNickname')
+        this.emailInput = document.querySelector('#uEmail')
+        this.phoneInput = document.querySelector('#uPhone')
+
+        this.nickReg = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$/;
+        this.emailReg =  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+        this.phoneReg = /^\d{3}-?\d{3,4}-?\d{4}$/;
+        this.pwReg = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$/;
 
         this.event()
     }
@@ -88,8 +98,14 @@ class SignupCheck{
         this.signupBtn.addEventListener('click', () => {
             if(this.checkTrue.style.display !== 'block'){
                 alert('아이디 중복확인을 해주세요.')
-            }else{
-                alert('회원가입에 성공하셨습니다.')
+            }else if(this.pwInput.value !== this.pwInput2.value){
+                alert('비밀번호가 일치하지 않습니다.')
+            }else if(!this.emailReg.test(this.emailInput.value)){
+                alert('이메일을 다시 확인해주세요.')
+            }else if(!this.phoneReg.test(this.phoneInput.value)){
+                alert('휴대폰 번호를 다시 입력해주세요.')
+            }else if((this.checkTrue.style.display === 'block') && (this.pwReg.test(this.pwInput.value)) && (this.pwInput.value === this.pwInput2.value) && (this.nickReg.test(this.nickInput.value)) && (this.emailReg.test(this.emailInput.value)) && (this.phoneReg.test(this.phoneInput.value))){
+                alert('회원가입에 성공했습니다.')
             }
         })
     }
