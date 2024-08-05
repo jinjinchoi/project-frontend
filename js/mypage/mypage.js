414db739bd21d981a79099e85e6b35b8c626e490
devingnow <https://github.com/devingnow>
2024-08-05 20:12:15 +0900

    0805 merge

--- js/mypage/mypage.js
+++ js/mypage/mypage.js
@@ -2,12 +2,17 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
       const response = await axios.get('http://localhost:3000/users/profile',{
         withCredentials: true});
-       // <USER_ID>를 실제 사용자 ID로 바꾸세요
-      const user = response.data;
 
-      document.querySelector('#user_img').src = user.uprofile || 'No profile';
+        
+        const user = response.data;
+        console.log(response)
+      if(user.uprofile){
+        document.querySelector('#user_img').src = user.uprofile;
+      }else{
+        document.querySelector('#user_img').src =  "../../profile/unknown.jpg"
+      }
       document.querySelector('#uid_content').textContent = user.uid;
-      document.querySelector('#umessage_content').textContent = user.umessage || 'No message';
+      document.querySelector('#umessage_content').textContent = '********';
       document.querySelector('#nick_content').textContent = user.unickname;
       document.querySelector('#email_content').textContent = user.uemail;
       document.querySelector('#phone_content').textContent = user.uphone;
